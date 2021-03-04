4159d9bb9eb8aeebcd61a05f9c11d3d68fd13cf2
Learn-endless <1983318249@qq.com>
2021-03-04 22:24:54 +0800

    C++

--- C++入门到精通/test_2/test_2/test_2_1.c
+++ C++入门到精通/test_2/test_2/test_2_1.c
@@ -6,10 +6,10 @@ int main()
 {
 	int i = 0;
 	int count = 0;
-	for (i = 101; i < 200; i+=2)
+	for (i = 101; i < 200; i += 2)
 	{
 		int j = 0;
-		for (j = 2; j <=sqrt(i); j++)
+		for (j = 2; j <= sqrt(i); j++)
 		{
 			if (i%j == 0)
 			{
